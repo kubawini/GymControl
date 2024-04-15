b4dcc611ea0c983770483c7f27b53c73d4492c84
kubawini <93740269+kubawini@users.noreply.github.com>
2024-04-15 15:04:00 +0200

    Initial commit

---
+++ algorithm.py
@@ -0,0 +1,23 @@
+import gym
+
+# Hyper parameters
+simulation_steps = 10
+population = 100
+
+def run():
+    raise NotImplemented
+
+def initialize_population(population:int):
+    raise NotImplemented
+
+def reproduce():
+    raise NotImplemented
+
+def crossing():
+    raise NotImplemented
+
+def mutation():
+    raise NotImplemented
+
+def succession():
+    raise NotImplemented
