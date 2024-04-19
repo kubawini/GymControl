00b32c6ad6d859b753bf62e75fe5c42af1549fe4
kubawini <kubawini@op.pl>
2024-04-19 17:04:57 +0200

    Repair play.py according to gym v0.25.2

--- src/play.py
+++ src/play.py
@@ -1,5 +1,6 @@
 import gym
 import pygame
 from gym.utils.play import play
-mapping = {(pygame.K_LEFT,): 0, (pygame.K_RIGHT,): 2}
-play(gym.make("MountainCar-v0", render_mode="rgb_array"), keys_to_action=mapping)
+
+mapping = {(pygame.K_LEFT,): 0, (pygame.K_RIGHT,): 1}
+play(gym.make("CartPole-v1"), keys_to_action=mapping)
